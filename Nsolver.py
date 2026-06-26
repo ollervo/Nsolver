38067b3029f8bbd4b806132945788a55d84e0529
ollervo <43470617+ollervo@users.noreply.github.com>
2026-06-26 11:06:58 +0300

    Update Nsolver.py
    Implemented multiple predicate symbols, with arbitrary arities. Syntax is, predicates with capital letters (C,D,E,F reserved for operators) and variables/constants with lower letters.
    
    Can now handle negation, by pushing to negation normal form.

--- Nsolver.py
+++ Nsolver.py
@@ -6,7 +6,7 @@ A = ["a","b","c","d","e","f"]
 
 R = {}
 
-pi = {}
+Q = {}
 
 for i in range(len(A)):
     R[A[i]]=i
@@ -15,6 +15,14 @@ for i in range(len(A)):
     else:
         R["-"+A[i]]=0
 
+for i in A:
+    for j in A:
+        Q[i+j]=1
+        Q["-"+i+j]=0
+
+
+
+pi = {"R":R,"Q":Q} #Example interpretation. Interpretation is a dir of predicates, which are themselves directories of the values for literals
         
             
 
@@ -39,20 +47,20 @@ class Nsolver:
                     else:
                         return 0
             else: #a is a predicate literal
-                if a[0]=="-":
-                    return self.pi["-"+a[2]]
-                else:
-                    return self.pi[a[1]]
-        #Assuming that formulae are already in negation normal form. Will maybe impliment later nnf function. Assuming also that parameter variables do not appear bounded by a quantifier.
+                L,v=self.literal_args(a)
+                return self.pi[L][v]
+        # Assuming that quantified variables do not appear as free variables.
         if a[0] == "D": #Disjunction is handled as addition
             b,c = self.subs(a)
             return self.evaluate(b,{})+self.evaluate(c,{})
         
-        if a[0] == "C": #Conjunction is handled as multipliation
+        if a[0] == "C": 
+            #Conjunction is handled as multipliation
             b,c = self.subs(a)
             return self.evaluate(b,{})*self.evaluate(c,{})
         
         if a[0]=="E": 
+            #Exists sums over the domain
             y,b = self.subs(a)
             sum = 0
             for i in self.A:
@@ -60,22 +68,61 @@ class Nsolver:
             return sum
         
         if a[0]=="F": 
+            #For all multiplies over domain
             y,b = self.subs(a)
             prod = 1
             for i in self.A:
                 prod*= self.evaluate(self.substitution(b,y,i),{})
             return prod
+        
+        if a[0]=="-":
+            #Push negation down eventually to literals
+            return self.evaluate(self.push_negation(a),{})
 
     def is_literal(self,a):
         return "(" not in a
     
-    def ground(self,a,x): #grounds the variables in a with a valuation function x
+    def literal_args(self,a):
+        # returns the relation symbol and variables of a literal. If negated literal, then returns "-" in front of variables
+        start = 0
+        for i in range(len(a)):
+            if a[i].islower():
+                start = i
+                break
+        if a[0]=="-":
+            return a[1:start],"-"+a[start:]
+        else:
+            return a[:start],a[start:]
+    
+    def ground(self,a,x): 
+        #grounds the variables in a with a valuation function x
         if len(x)==0:
             return a
         newformula = a
         for i in x.keys():
             newformula = self.substitution(newformula,i,x[i])
         return newformula
+    
+    def push_negation(self,a):
+        #Push negation down one subformula using De Morgan's laws. -D(A,B)<->C(-A,-B), -C(A,B)<->D(-A,-B), -E(x,A)<->F(x,-A) , -F(x,A)<->E(x,-A)
+        A,B = self.subs(a)
+        if B[0]=="-":
+            B=B[1:]
+        else:
+            B="-"+B
+        if A[0].isupper():
+            A = "-"+A
+        elif A[0]=="-":
+            A = A[1:]
+
+        if a[1]=="D":
+            return "C("+A+","+B+")"
+        if a[1]=="C":
+            return "D("+A+","+B+")"
+        if a[1]=="E":
+            return "F("+A+","+B+")"
+        if a[1]=="F":
+            return "E("+A+","+B+")"
 
     def substitution(self,a,x,b):
         # substitute all instances of x in formula a with b
@@ -89,7 +136,6 @@ class Nsolver:
 
     def subs(self,a):
         # returns subformulae
-        
         for i in range(len(a)):
             if a[i]=="(":
                 start=i
@@ -119,31 +165,16 @@ class Nsolver:
 
 
 if __name__ == "__main__":
-    solver = Nsolver(A,R)
+    solver = Nsolver(A,pi)
     psi1 = "D(Rb,Rc)"
     psi2 = "E(x,R(x))"
-    psi3 = "x=y"
-    psi4 = "C(Rb,Rc)"
+    psi3 = "D(-Qxy,Ry)"
     psi4 = "C(D(Rb,Rc),C(Rb,Rc))"
     psi5 = "E(x,x=x)"
     psi6 = "F(x,E(y,C(-x=y,Ry)))"
     psi7 = "E(x,E(y,C(C(-x=y,Ry),Rx)))"
-    print(solver.subs(psi1))
-    print(solver.subs(psi2))
-    print(solver.substitution(psi2,"x","a"))
-    print(solver.ground(psi3,{"x":"a","y":"a"}))
-    print(solver.evaluate("a=a",{}))
-    print(solver.evaluate("-a=b",{}))
-    print(solver.evaluate("Rx",{"x":"d"}))
-    print(solver.evaluate("Rb",{}))
-    print(solver.evaluate("Rc",{}))
-    print(solver.evaluate(psi1,{}))
+    print(solver.evaluate(psi3,{"x":"a","y":"b"}))
     print(solver.evaluate(psi4,{}))
     print(solver.evaluate(psi5,{}))
     print(solver.evaluate(psi6,{}))
     print(solver.evaluate(psi7,{}))
-        
-
-
-
-
